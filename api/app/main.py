4bf1db3fae77b936092cd680ddebcf3716ff055c
lukesj28 <lukesj28@gmail.com>
2025-10-15 20:29:05 -0400

    feat(api): add cors and prepare gitignore

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import users, follows, posts, likes
 
 app = FastAPI()
+
+origins = [
+    'http://localhost',
+    'http://localhost:80',
+    'http://localhost:5173'
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(follows.router, prefix="/follows", tags=["follows"])
